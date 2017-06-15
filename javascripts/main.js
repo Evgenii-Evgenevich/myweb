9f49390c216ba39fe3eda4489c8f7036e1d959fa
Evgenii Evgenyevich <eb_genius@mail.ru>
2017-06-15 13:55:37 +0300

    Create main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
   document.getElementById('showScroll').innerHTML = scrolled + 'px';
 }
 
-window.ontouchmove = function(evt) {
+window.ontouchend = function(evt) {
   if (typeof evt.touches === 'undefined') {
     return;
   }
@@ -12,7 +12,7 @@ window.ontouchmove = function(evt) {
   var d_x = evt.touches[0].clientX;
   var d_y = evt.touches[0].clientY;
   
-  if (Math.abs(d_x) > 50 && Math.abs(d_y) < 5) {
+  if (Math.abs(d_x) > 50) {
     document.getElementById('left_wrap').classList.add('menu-visible');
   }
 }
